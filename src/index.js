a5aca7284d6dd8041ef4a3df6fe1957a46a96405
Guilherme <gwgarcia21@gmail.com>
2024-03-10 15:24:33 -0300

    correções

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './fonts/font-awesome/css/font-awesome.css'
+import './bootstrap.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
